c1c3f8e0a1bfaef38401220abf87bbf1565bc9ec
peter86u <payalaortiz@gmail.com>
2021-03-14 00:31:14 -0300

    Cambio acceso a datos

--- config/db.php
+++ config/db.php
@@ -1,11 +1,11 @@
 <?php
-	
-define("DB_HOST", "localhost");//DB_HOST:  generalmente suele ser "127.0.0.1"
-define("DB_NAME", "test");//Nombre de la base de datos
-define("DB_USER", "root");//Usuario de tu base de datos
-define("DB_PASS", "admin");//Contraseña del usuario de la base de datos
 
 /*define("DB_HOST", "localhost");//DB_HOST:  generalmente suele ser "127.0.0.1"
+define("DB_NAME", "test");//Nombre de la base de datos
+define("DB_USER", "root");//Usuario de tu base de datos
+define("DB_PASS", "admin");//Contraseña del usuario de la base de datos*/
+
+define("DB_HOST", "localhost");//DB_HOST:  generalmente suele ser "127.0.0.1"
 define("DB_NAME", "admin_ventas");//Nombre de la base de datos
 define("DB_USER", "test");//Usuario de tu base de datos
-define("DB_PASS", "@dmin860809*");//Contraseña del usuario de la base de datos*/
+define("DB_PASS", "@dmin860809*");//Contraseña del usuario de la base de datos
